af1f5b8c3c1407c52912d851a4fce6ffaf8ddede
ohduran <alvaro.duranb@hotmail.com>
2020-05-01 13:36:58 +0200

    styling searchbox

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -12,12 +12,12 @@ const SearchBox = ({ currentRefinement, refine }) => (
       role="search"
       className="flex justify-center bg-white py-2 px-2 rounded-full shadow-lg"
     >
-      <MagnifyingGlass className="h-4 w-4 text-gray-900 my-auto" />
+      <MagnifyingGlass className="h-4 w-4 text-orange-900 my-auto" />
       <input
         type="search"
         value={currentRefinement}
         onChange={event => refine(event.currentTarget.value)}
-        className="mx-2 h-8 w-8/12 bg-gray-100 "
+        className="mx-2 h-8 w-8/12 bg-orange-200 text-orange-900 px-2"
       />
       {currentRefinement ? (
         <button className="mx-2 my-auto" onClick={() => refine("")}>
