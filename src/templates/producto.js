8d51690f624917184d48a2e984a91a8c435cd7cf
ohduran <alvaro.duranb@hotmail.com>
2020-05-09 15:48:03 +0200

    remove context

--- src/templates/producto.js
+++ src/templates/producto.js
@@ -1,17 +1,20 @@
-import React, { Component } from "react"
+import React, { useState } from "react"
 import Img from "gatsby-image"
 
 import { Default } from "../layouts/Default"
-import { ProductConsumer } from "../context"
 
 const ProductPage = ({ data: { shopifyProduct: product } }) => {
+  const variants = product.variants
+
+  const [variant, setVariant] = useState(variants[0])
+
   return (
     <Default>
       <main className="mt-2">
         <figure>
           <Img
             className="mx-2 h-64 object-cover max-h-full"
-            fluid={product.variants[0].image.localFile.childImageSharp.fluid}
+            fluid={variant.image.localFile.childImageSharp.fluid}
             alt={product.title}
           />
         </figure>
@@ -20,30 +23,22 @@ const ProductPage = ({ data: { shopifyProduct: product } }) => {
             {product.title}
           </h2>
           <h6 className="mt-5 text-xs line-through text-red-500">
-            {product.variants[0].compareAtPriceV2.amount >
-            product.variants[0].priceV2.amount
-              ? product.variants[0].compareAtPriceV2.amount +
+            {variant.compareAtPriceV2.amount > variant.priceV2.amount
+              ? variant.compareAtPriceV2.amount +
                 " " +
-                product.variants[0].compareAtPriceV2.currencyCode
+                variant.compareAtPriceV2.currencyCode
               : ""}
           </h6>
           <h3 className="text-3xl">
-            {product.variants[0].priceV2.amount}{" "}
-            {product.variants[0].priceV2.currencyCode}
+            {variant.priceV2.amount} {variant.priceV2.currencyCode}
           </h3>
-        </section>
-        <ProductConsumer>
-          {value => {
-            return <h1>{value}</h1>
-          }}
-        </ProductConsumer>
-        <section className="mx-5 mt-5 flex justify-evenly">
           <button className="bg-orange-300 hover:bg-orange-900 text-orange-900 hover:text-orange-300 focus:outline-none focus:bg-orange-900 px-6 py-3 rounded-lg shadow-lg text-lg">
-            {product.variants[0].availableForSale
+            {variant.availableForSale
               ? "Añadir a la Bolsa"
               : "Producto no disponible"}
           </button>
         </section>
+
         <section className="mx-8 mt-8">
           <p
             className="text-lg tracking-wide text-justify"
